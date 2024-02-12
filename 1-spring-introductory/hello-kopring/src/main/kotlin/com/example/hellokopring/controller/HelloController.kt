f2a6f6fc97ccf4e0f2ea10c7f561bb3cf139b9e1
mildsalmon <mildsalmon@gmail.com>
2024-02-12 10:28:25 +0900

    feat: Add first controller

--- 1-spring-introductory/hello-kopring/src/main/kotlin/com/example/hellokopring/controller/HelloController.kt
+++ 1-spring-introductory/hello-kopring/src/main/kotlin/com/example/hellokopring/controller/HelloController.kt
@@ -1,4 +1,15 @@
 package com.example.hellokopring.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class HelloController {
+
+    @GetMapping("hello")
+    fun hello(model: Model): String {
+        model.addAttribute("data", "mildsalmon")
+        return "hello"
+    }
 }
